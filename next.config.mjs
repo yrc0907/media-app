cbf7c71b76a007a8d219e00b768e584232ad26d1
余某人 <3616104980@qq.com>
2024-08-01 21:16:22 +0800

    first commit

---
+++ next.config.mjs
@@ -0,0 +1,29 @@
+/** @type {import('next').NextConfig} */
+import process from 'process';
+const nextConfig = {
+  experimental: {
+    staleTimes: {
+      dynamic: 30,
+    },
+  },
+  serverExternalPackages: ["@node-rs/argon2"],
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "utfs.io",
+        pathname: `/a/${process.env.NEXT_PUBLIC_UPLOADTHING_APP_ID}/*`,
+      },
+    ],
+  },
+  rewrites: () => {
+    return [
+      {
+        source: "/hashtag/:tag",
+        destination: "/search?q=%23:tag",
+      },
+    ];
+  },
+};
+
+export default nextConfig;
